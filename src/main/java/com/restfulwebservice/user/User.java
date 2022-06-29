c2424466a7ed75cd09b1ac52986602358422b7a3
0sang <dls33_@naver.com>
2022-06-29 22:35:43 +0900

    Spring Boot를 이용한 RESTful Web Services 개발 (~섹션3 / XML format)

--- src/main/java/com/restfulwebservice/user/User.java
+++ src/main/java/com/restfulwebservice/user/User.java
@@ -3,8 +3,8 @@ package com.restfulwebservice.user;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Data
@@ -13,7 +13,9 @@ public class User {
 
     private Integer id;
 
+    @Size(min = 2, message = "Name은 2글자 이상 입력해 주세요.")
     private String name;
 
+    @Past // 과거의 date만 들어감
     private Date joinDate;
 }
